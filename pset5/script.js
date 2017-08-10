d92edc162600332e70ae81a477cbf31170dc227e
defunktt1 <vurado3@gmail.com>
2017-08-10 17:19:56 +0300

    Realtime update

--- pset5/script.js
+++ pset5/script.js
@@ -1,60 +1,72 @@
 const ONE_HOUR = 3600000;
 lastMsgTimestamp = 0;
+
 $(document).ready(function () {
 
+    // load history from json file
     historyLoad();
+    // realtime update if user sends new message from another browser
+    realtimeUpdate();
 
     $("#message_submit_button").click(function (e) {
         e.preventDefault();
-
         var currentTime = new Date();
+
         $.post("chat_data.php", {ts: currentTime.getTime(), msg: $("#message").val()},
             function (data) {
-                var key;
-                var table = [];
-                for (key in data) {
+                for (var key in data) {
                     if (data[key].time > lastMsgTimestamp) {
+                        var table = [];
                         table.push([(data[key].time), data[key].user, data[key].message]);
                     }
                 }
-                for (var i = 0; i < table.length; i++) {
-                    if (i == table.length - 1) {
+
+                var arrayLength = table.length;
+                for (var i = 0; i < arrayLength; i++) {
+                    if (i === arrayLength - 1) {
                         lastMsgTimestamp = table[i][0];
                     }
+
                     table[i][0] = '[' + getCurrentTime(table[i][0]) + ']';
                     table[i][1] = '<b>' + table[i][1] + '</b>' + ": ";
                     table[i] = table[i].join(" ") + '<br /><br />';
                 }
+
                 var chat = $(".chat-window");
-                $("#message").val("");
+                $("#message").val(""); // clear input field
                 chat.append(table);
                 chat.scrollTop(chat[0].scrollHeight);
             });
     });
 });
 
+
 function historyLoad() {
     $.post("chat_data.php", {}, function (data) {
+
         var currentTime = new Date();
-        var key;
         var table = [];
-        for (key in data) {
+        for (var key in data) {
             if (currentTime.getTime() - data[key].time <= ONE_HOUR) {
                 table.push([data[key].time, data[key].user, data[key].message]);
             }
         }
-        for (var i = 0; i < table.length; i++) {
-            if (i == table.length - 1) {
+
+        var arrayLength = table.length;
+        for (var i = 0; i < arrayLength; i++) {
+            if (i === arrayLength - 1) {
                 lastMsgTimestamp = table[i][0];
             }
+
             table[i][0] = '[' + getCurrentTime(table[i][0]) + ']';
             table[i][1] = '<b>' + table[i][1] + '</b>' + ": ";
             table[i] = table[i].join(" ") + '<br><br>';
         }
 
-        if (lastMsgTimestamp == 0) {
+        if (lastMsgTimestamp === 0) {
             lastMsgTimestamp = currentTime.getTime();
         }
+
         var chat = $(".chat-window");
         chat.html(table);
         chat.scrollTop(chat[0].scrollHeight);
@@ -79,3 +91,30 @@ function getCurrentTime(localTime) {
     return hours + ':' + minutes + ':' + seconds;
 }
 
+function realtimeUpdate() {
+    setInterval(function () {
+        $.post("chat_data.php", {}, function (data) {
+            var table = [];
+
+            for (var key in data) {
+                if (data[key].time > lastMsgTimestamp) {
+                    table.push([(data[key].time), data[key].user, data[key].message]);
+                }
+            }
+
+            var arrayLength = table.length;
+            for (var i = 0; i < arrayLength; i++) {
+                if (i === arrayLength - 1) {
+                    lastMsgTimestamp = table[i][0];
+                }
+
+                table[i][0] = '[' + getCurrentTime(table[i][0]) + ']';
+                table[i][1] = '<b>' + table[i][1] + '</b>' + ": ";
+                table[i] = table[i].join(" ") + '<br /><br />';
+            }
+
+            var chat = $(".chat-window");
+            chat.append(table);
+        });
+    }, 1000) // 1 sec update delay
+}
